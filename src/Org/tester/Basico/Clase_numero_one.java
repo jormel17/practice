aa6b16a82c6bb6ef6ee5d721c669f0818e3ad29f
Jormel <jormel17>
2020-02-20 16:08:21 -0300

    Primer commit

---
+++ src/Org/tester/Basico/Clase_numero_one.java
@@ -0,0 +1,33 @@
+package Org.tester.Basico;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Clase_numero_one {
+    public static void main (String[] args) {
+        //instanciar un objeto de tipo webdriver
+        WebDriver driver;
+
+        //Declarar variables
+        String BaseUrl = "http://newtours.demoaut.com/";
+        String actualResult = "";
+        String expectedResult = "";
+
+        //Indicar la localización del archivo chromedriver.exe en la propiedad webdriver.chrome
+        //System.getproperty ("user.dir") = C: \..\..\..\SeleniumWebDriver
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\Drivers\\chromedriver.exe");
+
+        //Abrir el navegador
+        driver=new ChromeDriver();
+
+        //Navegar a la pagina
+        driver.get(BaseUrl);
+
+        //Obtener titulo
+        actualResult = driver.getTitle();
+
+        driver.close();
+
+    }
+
+}
---
+++ src/Org/tester/Intermedio/dataProvider.java
@@ -0,0 +1,26 @@
+package Org.tester.Intermedio;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+public class dataProvider {
+    WebDriver driver;
+
+    @BeforeTest
+    public void setup(){
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\Drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.google.com/");
+    }
+
+    @AfterTest
+    public void Teardowm(){
+        driver.quit();
+    }
+}
