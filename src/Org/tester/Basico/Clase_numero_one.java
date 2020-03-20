3def9e0433e6dbf41e3eec17ad1b9811fe507747
Jormel <jormel17>
2020-03-20 14:45:00 -0300

    Nuevas practicas realizadas

--- src/Org/tester/Basico/Clase_numero_one.java
+++ src/Org/tester/Basico/Clase_numero_one.java
@@ -2,6 +2,8 @@ package Org.tester.Basico;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 public class Clase_numero_one {
     public static void main (String[] args) {
